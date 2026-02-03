8efc2e36fa344b57d269afcb54be8916327f9380
Froggles <elmsterm@gmail.com>
2026-02-03 10:42:39 -0500

    render a message when no show is selected

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -3,6 +3,9 @@ import "./shows.css";
 
 /** Allows users to browse through the episodes of the given show */
 export default function ShowDetails(show) {
+  if (!show) {
+    return <p>Select a show to see details</p>;
+  }
   const [selectedEpisode] = useState();
   return <div className="show-details"></div>;
 }
