06b0aaf8f7299dcb1034e28d814190f6e8664243
Froggles <elmsterm@gmail.com>
2026-02-03 10:50:19 -0500

    pass props to EpisodeList and EpisodeDetails in ShowDetails

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,11 +1,24 @@
 import { useState } from "react";
 import "./shows.css";
+import EpisodeList from "../episodes/EpisodeList";
+import EpisodeDetails from "../episodes/EpisodeDetails";
 
 /** Allows users to browse through the episodes of the given show */
 export default function ShowDetails(show) {
   if (!show) {
     return <p>Select a show to see details</p>;
   }
-  const [selectedEpisode] = useState();
-  return <div className="show-details"></div>;
+
+  const [selectedEpisode, setSelectedEpisode] = useState();
+  return (
+    <div className="show-details">
+      <EpisodeList
+        name={show.name}
+        episodes={show.episodes}
+        selectedEpisode={selectedEpisode}
+        setSelectedEpisode={setSelectedEpisode}
+      ></EpisodeList>
+      <EpisodeDetails episode={selectedEpisode}></EpisodeDetails>
+    </div>
+  );
 }
