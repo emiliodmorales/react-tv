ab56d37521e8bb0f3336c572679e3ae121ac6bb7
Froggles <elmsterm@gmail.com>
2026-02-03 10:55:17 -0500

    render each show as an a tag

--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -2,5 +2,13 @@ import "./shows.css";
 
 /** A navbar that allows users to choose between a list of shows */
 export default function ShowSelection({ shows, setSelectedShow }) {
-  return <nav className="shows"></nav>;
+  return (
+    <nav className="shows">
+      {shows.map((show) => {
+        <a class="show" key={show.name} onClick={setSelectedShow(show)}>
+          {show.name}
+        </a>;
+      })}
+    </nav>
+  );
 }
