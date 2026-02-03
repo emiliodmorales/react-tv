b78c2809b049264c6e546c085f35ee9adecb0693
Froggles <elmsterm@gmail.com>
2026-02-03 10:40:53 -0500

    keep the selected episode in a state variable in ShowDetails

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import "./shows.css";
 
 /** Allows users to browse through the episodes of the given show */
 export default function ShowDetails(show) {
+  const [selectedEpisode] = useState();
   return <div className="show-details"></div>;
 }
