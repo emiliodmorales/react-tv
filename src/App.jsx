704f71ec3db7afdd1f30df811a5873be4adeb9e2
Froggles <elmsterm@gmail.com>
2026-02-03 10:57:17 -0500

    import tvShows from data into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { tvShows } from "./shows/data";
+
 /**
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
 export default function App() {
+  const [shows] = useState(tvShows);
   return (
     <>
       <header>
