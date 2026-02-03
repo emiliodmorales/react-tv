a93fd4430a0ebcca6e3c994f2712fd4e37bb7976
Froggles <elmsterm@gmail.com>
2026-02-03 10:53:53 -0500

    take props in ShowSelection

--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,6 +1,6 @@
 import "./shows.css";
 
 /** A navbar that allows users to choose between a list of shows */
-export default function ShowSelection() {
+export default function ShowSelection({ shows, setSelectedShow }) {
   return <nav className="shows"></nav>;
 }
